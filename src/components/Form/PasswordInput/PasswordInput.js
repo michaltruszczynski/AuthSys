2459a28c4eeaa200526089df5d13e27b9783fddc
michaltruszczynski <michal.truszczynski2@gmail.com>
2020-08-09 20:02:08 +0200

    working tooltip positioning concept

--- src/components/Form/PasswordInput/PasswordInput.js
+++ src/components/Form/PasswordInput/PasswordInput.js
@@ -10,44 +10,54 @@ const PasswordInput = ({ id, elementType, config, value, invalid, touched, chang
 
     const [isInputActive, setIsInputActive] = useState(false);
     const [coords, setCoords] = useState();
-    const size = useWindowSize();
     const passwordInputElementRef = useRef();
+    const tooltipRef = useRef();
 
-    useLayoutEffect(() => {
-        console.log('Password input DidMount');
-        function handlePosition() {
-            const coordsRefEl = passwordInputElementRef.current.getBoundingClientRect();
-            const { height, width, top, left } = coordsRefEl;
-            const tooltipCoords = {
-                top: top + window.scrollY + Math.floor(height / 2),
-                left: left + width
-            };
-            console.log('tooltipCoords', tooltipCoords);
-            setCoords(tooltipCoords)
-        }
+    const size = useWindowSize();
+
+    function handlePosition() {
+        const coordsRefEl = passwordInputElementRef.current.getBoundingClientRect();
+        const { height, width, top, left } = coordsRefEl;
+        const tooltipCoords = {
+            top: top + window.scrollY + Math.floor(height / 2),
+            left: left + width
+        };
+        console.log('tooltipCoords', tooltipCoords);
+        console.log('coordsRefEl', coordsRefEl)
+        // setCoords(tooltipCoords)
+        tooltipRef.current.style.top = tooltipCoords.top + 'px';
+        tooltipRef.current.style.left = tooltipCoords.left + 'px';
+       
+    }
+
+
+
+    useEffect(() => {
+        console.log('[componentDidMount] Password input ');
+
 
         handlePosition();
-        window.addEventListener('resize', handlePosition)
+        window.addEventListener('resize', handlePosition);
         return () => window.removeEventListener('resize', handlePosition)
-    }, []);
+    }, [handlePosition]);
 
 
     const focusHandler = () => {
         setIsInputActive(!isInputActive);
     };
 
-    console.log(isInputActive);
+    // console.log(isInputActive);
 
     let inputClasses = ["form__input"];
     if (!invalid && touched) {
         inputClasses.push("form__input--error")
     }
-    console.log('isFocused', isFocused, coords);
+    // console.log('isFocused', isFocused, coords);
+    console.log('size', size)
 
     return (
         <div className="form__item">
             <Resize />
-            <p>{size.width}</p>
             <label htmlFor={id} className="form__label">{config.label}: </label>
             <input
                 className={inputClasses.join(' ')}
@@ -63,7 +73,7 @@ const PasswordInput = ({ id, elementType, config, value, invalid, touched, chang
             />
             <span className="form__input-error">{errorMsg}</span>
             {}
-            {true && <ToolTip position={coords}>
+            {true && <ToolTip ref={tooltipRef}>
                 <PasswordValidator value={value} active={isInputActive} type={'tooltip'} />
             </ToolTip>}
         </div>
--- src/components/Tooltip/Tooltip.js
+++ src/components/Tooltip/Tooltip.js
@@ -3,23 +3,22 @@ import React from 'react';
 import Portal from '../UI/Portal/Portal';
 import styles from './Tooltip.module.css';
 
-const Tooltip = ({ position, children }) => {
-    if (!position) return null;
+const Tooltip = React.forwardRef(({ children }, ref) => {
+    // if (!position) return null;
 
+    // const styleCoords = {
+    //     top: position.top + 'px',
+    //     left: position.left + 'px'
+    // }
 
-    const styleCoords = {
-        top: position.top + 'px',
-        left: position.left + 'px'
-    }
-
-
+    console.log('Tooltip, top:', ref)
     return (
         <Portal>
-            <div className={styles.tooltip} style={styleCoords} >
+            <div className={styles.tooltip} ref={ref} >
                 {children}
             </div>
         </Portal>
     )
-}
+})
 
 export default Tooltip;
