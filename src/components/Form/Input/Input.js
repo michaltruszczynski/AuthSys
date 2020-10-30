4f9f1a42c92bbc24bddd9fbdd97dc8c4883af65e
michaltruszczynski <michal.truszczynski2@gmail.com>
2020-10-30 13:05:00 +0100

    refactored input css

--- src/components/Form/Input/Input.js
+++ src/components/Form/Input/Input.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
-import './Input.css';
+import styles from './Input.module.css';
 
 const Input = ({ id, elementType, config, value, invalid, touched, changed, errorMsg }) => {
 
     let inputElement = null;
 
-    let inputClasses = ["form__input"];
+    let inputClasses = [styles.Form__input];
     if (!invalid && touched) {
-        inputClasses.push("form__input--error")
+        inputClasses.push(styles['Form__input--error'])
     }
 
     switch (elementType) {
@@ -25,10 +25,10 @@ const Input = ({ id, elementType, config, value, invalid, touched, changed, erro
     }
 
     return (
-        <div className="form__item">
-            <label htmlFor={id} className="form__label">{config.label}: </label>
+        <div className={styles.Form__item}>
+            <label htmlFor={id} className={styles.Form__label}>{config.label}: </label>
             {inputElement}
-            <span className="form__input-error">{errorMsg}</span>
+            <span className={styles['Form__input-error']}>{errorMsg}</span>
         </div>
     )
 }
